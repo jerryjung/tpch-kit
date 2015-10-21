458a4b868cca02a443d8b56b5d09fd4a5b06dc04
jerryjung <lovecraft@naver.com>
2015-10-21 23:10:15 +0900

    modified properties

--- report_set/properties.py
+++ report_set/properties.py
@@ -1,25 +1,18 @@
 #Test Home directory
-#home_dir="/home/hadoop/tpch_query/tajo"
 #home_dir="/home/hadoop/tpch_query/hive"
 #home_dir="/home/hadoop/tpch_query/spark"
-#home_dir="/home/hadoop/tpch_query/impala"
 
 #Test Runner
-#test_runner="Tajo"
 #test_runner="Hive"
-#test_runner="Impala"
 #test_runner="Spark"
 
 #Master Host Name
 host_name="jnn-g07-02"
-#host_name="jnn-g06-01"
 
 #Server Config
 os_version="CentOS 6.6"
 hadoop_version="2.6.0"
-#runner_version="Tajo : 0.11.0"
 hive_version="HIVE : 1.1.0, Tez : 0.6.0-minimal"
-#runner_version="Impala : 2.1.2-cdh5 RELEASE"
 spark_version="Spark : 1.4.0"
 
 #chart report subject
